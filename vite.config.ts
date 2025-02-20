2e4cce7c33933756c14f7b03a93c7a4cb70e85f0
Rifqi121 <rfauzanadzikra2.siswa@smkn4bdg.sch.id>
2025-02-21 00:03:58 +0700

    add config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['react-scroll','swiper/react','swiper/modules','swiper/css']
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-scroll','swiper/react','swiper/modules', 'swiper/css'],
+    },
+  },
 })
